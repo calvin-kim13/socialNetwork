3a913d5d4072d638d2ca5ee726827df2b4ad860c
Calvin Kim <calvinkim@Calvins-Air.lan>
2022-05-23 14:06:30 -0700

    added server and connection to mongo

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
+});
